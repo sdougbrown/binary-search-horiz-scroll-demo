dfa73ddd164be5e7845b0bdda0104dfbb82afa87
Doug Brown <doug@github.douggo.com>
2024-10-04 11:02:43 -0400

    ⏭️  Base App Layout
    Just throwing some things together so that the gallery isn't stuck at
    the top of the screen. It'll make more sense in another commit or two.

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,39 @@ import './App.scss'
 function App() {
 
   return (
-    <>
-      <Gallery />
-    </>
+    <main>
+      <div className="app-content">
+        <div className="app-content__wrap">
+          <h1>Gallery Scrolling</h1>
+        </div>
+      </div>
+      <div className="app-content--alt">
+        <div className="app-content__wrap">
+          <h3>
+            What are we doing here?
+          </h3>
+          <p className="app-content__wrap--half">{`
+            This is an experiment to support manual/native scrolling along with the common
+            scroll left/right buttons for desktop. Ideally I can turn this into a standalone module. We'll see.
+            `}
+          </p>
+        </div>
+      </div>
+      <div className="app-content">
+        <div className="app-content__wrap">
+          <h3>
+            Gallery Demo:
+          </h3>
+        </div>
+        <Gallery />
+      </div>
+
+      <div className="app-content--alt">
+        <div className="app-content__wrap">
+          <h4>Thanks for checking it out!</h4>
+        </div>
+      </div>
+    </main>
   )
 }
 
